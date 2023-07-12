26692a7105d5025e1b32094cec9933e468d14b8e
Alex-Tsarenko <ga4yt324@gmail.com>
2023-07-12 19:27:10 +0300

    fixes in Parser

---
+++ interpreter/SExpr.h
@@ -0,0 +1,117 @@
+#pragma once
+
+#include <iostream>
+
+//---------------------------------------------------------------
+//
+// SExpr - symbolic expression
+//
+//---------------------------------------------------------------
+//
+//  Example of how implemented s-expression: ( a b )
+//
+//  !-------------!        !-------------!
+//  ! SExpr: LIST !   +--->! SExpr: LIST !
+//  !.............!   |    !.............!
+//  ! CAR  ! CDR  !---+    ! CAR  ! CDR  !-->(nullptr)
+//  !------!------!        !------!------!
+//     |                      |
+//     V                      V
+//  !-----------------!    !-----------------!
+//  ! SExpr: ATOM     !    ! SExpr: ATOM     !
+//  !.................!    !.................!
+//  ! value ! name: a !    ! value ! name: b !
+//  !-------!---------!    !-------!---------!
+//
+//---------------------------------------------------------------
+//
+struct SExpr
+{
+    enum Type {
+        LIST=0,
+        ATOM
+    };
+
+    Type    m_type = LIST;
+
+    // CAR
+    union {
+        SExpr*      m_car;
+        SExpr*      m_atomValue;
+    };
+
+    // CDR
+    union {
+        SExpr*      m_next;
+        const char* m_atomName;
+    };
+
+
+    // NIL - "()"
+    SExpr() : m_type(LIST), m_car(nullptr), m_next(nullptr)
+    {}
+
+    // "( carValue )"
+    SExpr( SExpr* carValue ) : m_type(LIST), m_car(carValue), m_next(nullptr)
+    {}
+
+    // atom
+    SExpr( const char* atomName ) : m_type(ATOM), m_atomValue(this),  m_atomName(atomName) {
+        auto len = std::strlen(atomName)+1;
+        m_atomName = new char[ len ];
+        std::memcpy( (char*)m_atomName, atomName, len );
+    }
+
+    ~SExpr() {
+        if ( m_type == ATOM ) {
+            if ( m_atomName == nullptr )
+            {
+                delete [] m_atomName;
+            }
+        }
+    }
+
+    bool isNil() const { return m_type==LIST && m_car == nullptr && m_next==nullptr; }
+
+    void setValue( SExpr* value ) { m_car = value; }
+
+    void print( const char* prefix = nullptr ) const
+    {
+        if ( prefix != nullptr )
+        {
+            std::cout << prefix;
+        }
+        
+        switch (m_type)
+        {
+            case LIST:
+            {
+                if ( m_car == nullptr && m_next == nullptr )
+                {
+                    std::cout << "NIL " << std::flush;
+                    break;
+                }
+
+                std::cout << "( " << std::flush;
+                for( const auto* it = this; it != nullptr; it = it->m_next )
+                {
+                    if ( it->m_car == nullptr && m_next != nullptr )
+                    {
+                        std::cout << "NIL " << std::flush;
+                    }
+                    else
+                    {
+                        it->m_car->print();
+                    }
+                }
+                std::cout << " )" << std::flush;
+                break;
+            }
+            case ATOM:
+            {
+                std::cout << m_atomName << ' ' << std::flush;
+                break;
+            }
+        }
+    }
+};
