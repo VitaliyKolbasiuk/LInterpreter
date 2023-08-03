929428017a53db331a6ee156f6408696aadd1d53
Vitaliy Kolbasiuk <vitaliykolbasiuk05@gmail.com>
2023-08-03 22:55:37 +0300

    added mouse press event

--- Vitaliy/main.cpp
+++ Vitaliy/main.cpp
@@ -5,30 +5,36 @@
 #include <QPainter>
 #include <QVBoxLayout>
 #include <QTimer>
+#include <QMouseEvent>
 
-class CircleWidget: public QWidget{
+class CircleWidget : public QWidget {
 public:
     CircleWidget(QWidget *parent = nullptr) : QWidget(parent) {
         timer = new QTimer(this);
         connect(timer, &QTimer::timeout, this, QOverload<>::of(&CircleWidget::update));
         timer->start(10);
     }
+
 private:
     double x = 0, y = 0;
-    double dx = 2;
-    double dy = 2;
-    QTimer* timer;
+    double dx = 3;
+    double dy = 3;
+    QTimer *timer;
+    bool isMousePressed;
+    QPoint mousePressPos;
+    QPoint ellipsePos;
+
 protected:
-    void paintEvent(QPaintEvent* event) override{
+    void paintEvent(QPaintEvent *event) override {
         Q_UNUSED(event);
         QPainter painter(this);
         painter.setRenderHint(QPainter::Antialiasing, true);
 
         int radius = qMin(width(), height()) / 4;
-        if (x + dx > width() - radius || x + dx < 0){
+        if (x + dx > width() - radius || x + dx < 0) {
             dx = -dx;
         }
-        if (y + dy > height() - radius || y + dy < 0){
+        if (y + dy > height() - radius || y + dy < 0) {
             dy = -dy;
         }
         x += dx;
@@ -38,6 +44,29 @@ protected:
         painter.drawRect(0, 0, width(), height());
         painter.setBrush(Qt::blue);
         painter.drawEllipse(x, y, radius, radius);
+
+        int ellipseRadius = 30;
+        painter.setBrush(Qt::red);
+        painter.drawEllipse(50, ellipsePos.y(), 30, 100);
+    }
+
+    void mousePressEvent(QMouseEvent *event) override {
+        if (event->button() == Qt::LeftButton) {
+            isMousePressed = true;
+            mousePressPos = event->pos();
+        }
+    }
+
+    void mouseMoveEvent(QMouseEvent *event) override {
+        if (isMousePressed) {
+            ellipsePos = event->pos();
+        }
+    }
+
+    void mouseReleaseEvent(QMouseEvent *event) override {
+        if (event->button() == Qt::LeftButton) {
+            isMousePressed = false;
+        }
     }
 };
 
